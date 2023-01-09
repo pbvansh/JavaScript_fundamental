6853d9b08972614ab1040e5fe90e8f19fddade93
pbvansh <vanshpratik0165@gmail.com>
2023-01-09 17:21:48 +0530

    inital commit

---
+++ 3.js
@@ -0,0 +1,7 @@
+inputString1 = [{'col1': 'a', 'col2': 'b'}, {'col1': 'c', 'col2': 'd'}]
+let str = Object.keys(inputString1[0]).toString().concat("\\n")
+inputString1.map((obj)=>{
+    str = str + ","+Object.values(obj).toString().concat("\\n");
+})
+str = str.slice(0,-2)
+console.log(str);
