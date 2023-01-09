1c7a79df727865bc0429fdcbc54f7630fe8c0ff0
pbvansh <vanshpratik0165@gmail.com>
2023-01-09 16:32:39 +0530

    initial commit

---
+++ 1.js
@@ -0,0 +1,4 @@
+obj1 = { hair: 'long', beard: true }
+obj2 = { hair: 'long', beard: true }
+
+JSON.stringify(obj1) === JSON.stringify(obj2) ? console.log(true) : console.log(false);
---
+++ 2.js
@@ -0,0 +1,19 @@
+inputString = 'col1,col2\na,b\nc,d'
+// console.log(inputString);
+// console.log(Object.assign(inputString))
+
+let row1 = inputString.split('\n');
+const title = row1[0].split(',')
+const data = row1.splice(1);
+// console.log(data);
+// console.log(title);
+let obj = [];
+for (let i = 0; i < data.length; i++) {
+    const value = data[i].split(',');
+    let newObj = {}
+    for (let i = 0; i < value.length; i++) {
+        newObj[title[i]] = value[i]
+    }
+    obj.push(newObj)
+}
+console.log(obj); // [ { col1: 'a', col2: 'b' }, { col1: 'c', col2: 'd' } ]
